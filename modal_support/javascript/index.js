b51f77d7ff164e4c76d3b8dcd84bd1cd22e5fd0c
SasMatilda <matveychik.malakhov.03@mail.ru>
2024-01-22 13:10:00 +1000

    Completion of development of adaptive modal windows faq, support.

--- modal_support/javascript/index.js
+++ modal_support/javascript/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var modalSupport = document.getElementById("modal-support");
   var modalBoxSupport = document.getElementById("modal-support__box");
   var openBtn = document.getElementById("open-modal-support-btn");
+  var closeBtnSupport = document.getElementById("close-modal-support-btn");
 
   function openModal() {
     modalSupport.classList.add("open");
@@ -11,9 +12,16 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   openBtn.addEventListener("click", openModal);
+  closeBtnSupport.addEventListener("click", closeModal);
 
   window.addEventListener("keydown", function (e) {
     if (e.key === "Escape") {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!modalBoxSupport.contains(e.target) && e.target !== openBtn) {
       closeModal();
     }
   });
